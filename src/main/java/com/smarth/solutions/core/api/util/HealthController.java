5b1bfde1107754e30f0e25c98dc0a75c590c2140
Deybi-hud <deyb.ramirez@duocuc.cl>
2026-06-10 15:35:06 -0400

    fix: Correción de deploy y dockerfile

---
+++ src/main/java/com/smarth/solutions/core/api/util/HealthController.java
@@ -0,0 +1,23 @@
+package com.smarth.solutions.core.api.util;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/healths")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> health() {
+        Map<String, String> status = new HashMap<>();
+        status.put("status", "UP");
+        status.put("timestamp", Instant.now().toString());
+        return ResponseEntity.ok(status);
+    }
+}
